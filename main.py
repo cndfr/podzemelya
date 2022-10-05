97d7161baf72f8f6392026cff3c68868b927946f
cndfr <brok.lab@gmail.com>
2022-10-06 01:08:54 +0300

    works

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 import re
 import telebot
+import shelve
+import pickle
 
 bot = telebot.TeleBot('***REMOVED***')
 
 with open('/Users/cndfr/Library/Mobile Documents/com~apple~CloudDocs/DEV/Podzemelya/base.txt', newline='') as source:
     pages = source.readlines()
 
-# page restriction & exclusions
 
-
-def get_moves(id, text):
-    global players
-    players[id]['moves'] = [int(move) for move in re.findall(r'\b\d+\b', text)]
-
-# player
+# players
 
 
 players = {}
@@ -29,46 +25,67 @@ def create_character(id):
     global players
     players[id] = character
 
+    # with open("/Users/cndfr/Library/Mobile Documents/com~apple~CloudDocs/DEV/Podzemelya/data.pickle", "wb") as userdata:
+    #     pickle.dump(character, userdata, protocol=pickle.HIGHEST_PROTOCOL)
+    # with shelve.open('/Users/cndfr/Library/Mobile Documents/com~apple~CloudDocs/DEV/Podzemelya/userdata.dat', 'w') as userdata:
+    #     userdata[id] = character
+
+# pages -------------- restriction ------------- add exclusions
+
+
+# def create_page(i):
+#     page = {
+#         'items': '',
+#         'fight': False,
+#         'secmoves': '',
+#         'text': 'Вы быстро идете вперед — 3.',
+#     }
+#     global pages
+#     pages[i] = page
+
+
+def get_moves(id, text):
+    global players
+    players[id]['moves'] = [int(move) for move in re.findall(r'\b\d+\b', text)]
+
 # items
 
 # fights
 
 
 def generate_answer(message):
-    if message.text.isnumeric():
-        reqpage = int(message.text)
-        if reqpage > 0 and reqpage <= 617:
-            if reqpage in players[message.from_user.id]['moves']:
-                text = pages[reqpage].replace('/', '\r\n')       # remove this
-                get_moves(message.from_user.id, text)
-                return f'{text}'
-            else:
-                return 'Вы не можете сюда попасть'
-        else:
-            return 'Такой страницы нет'
-    else:
+    if not message.text.isnumeric():
         return 'Введите номер страницы'
+    reqpage = int(message.text)
+    if not (reqpage > 0 and reqpage <= 617):
+        return 'Такой страницы нет'
+    if reqpage not in players[message.from_user.id]['moves']:
+        return 'Вы не можете сюда попасть'
+
+    text = pages[reqpage].replace('<br>', '\r\n')
+    get_moves(message.from_user.id, text)
+    return f'{text}'
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
     create_character(message.from_user.id)
     get_moves(message.from_user.id, pages[1])
-    text = pages[1].replace('/', '\r\n')                         # remove this
+    text = pages[1].replace('<br>', '\r\n')
     bot.send_message(
-        message.chat.id, text, parse_mode='Markdown')
+        message.chat.id, text, parse_mode='Html')
 
 
 @bot.message_handler(commands=['debug'])
 def debug(message):
     bot.send_message(
-        message.chat.id, f'Players: {players}', parse_mode='Markdown')
+        message.chat.id, f'Players: {players}', parse_mode='Html')
 
 
 @bot.message_handler()
 def get_user_text(message):
     bot.send_message(
-        message.chat.id, generate_answer(message), parse_mode='Markdown')
+        message.chat.id, generate_answer(message), parse_mode='Html')
 
 
 bot.polling(non_stop=True)
