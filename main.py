24163773c8fd0aec4dc8aa6d9e9d2c0cb1e4b6f8
cndfr <brok.lab@gmail.com>
2022-10-06 13:47:11 +0300

    code command, local env, shorten paths

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import re
 import telebot
 import shelve
-import pickle
 
 bot = telebot.TeleBot('5752337489:AAGabYmTALazbxrgIVbkoyS2LFNSxHZjSf0')
 
-with open('/Users/cndfr/Library/Mobile Documents/com~apple~CloudDocs/DEV/Podzemelya/base.txt', newline='') as source:
+with open('base.txt', newline='') as source:
     pages = source.readlines()
 
 
@@ -22,7 +21,7 @@ def create_character(id):
         'spells': 0,
         'moves': 0,
     }
-    with shelve.open('/Users/cndfr/Library/Mobile Documents/com~apple~CloudDocs/DEV/Podzemelya/userdata', 'w') as userdata:
+    with shelve.open('userdata', 'w') as userdata:
         userdata[f'{id}'] = character
 
 # pages -------------- restriction ------------- add exclusions
@@ -40,7 +39,7 @@ def create_character(id):
 
 
 def get_moves(id, text):
-    with shelve.open('/Users/cndfr/Library/Mobile Documents/com~apple~CloudDocs/DEV/Podzemelya/userdata', 'w') as userdata:
+    with shelve.open('userdata', 'w') as userdata:
         profile = userdata[f'{id}']
         profile['moves'] = [int(move) for move in re.findall(r'\b\d+\b', text)]
         userdata[f'{id}'] = profile
@@ -56,13 +55,13 @@ def generate_answer(message):
     reqpage = int(message.text)
     if not (reqpage > 0 and reqpage <= 617):
         return 'Такой страницы нет'
-    with shelve.open('/Users/cndfr/Library/Mobile Documents/com~apple~CloudDocs/DEV/Podzemelya/userdata', 'w') as userdata:
+    with shelve.open('userdata', 'r') as userdata:
         profile = userdata[f'{message.from_user.id}']
         moves = profile['moves']
-    if reqpage == moves[0]:
-        return 'Вы сейчас здесь'
     if reqpage not in moves:
         return 'Вы не можете сюда попасть'
+    if reqpage == moves[0]:
+        return 'Вы сейчас здесь'
 
     text = pages[reqpage].replace('<br>', '\r\n')
     get_moves(message.from_user.id, text)
@@ -80,7 +79,7 @@ def start(message):
 
 @bot.message_handler(commands=['debug'])
 def debug(message):
-    with shelve.open('/Users/cndfr/Library/Mobile Documents/com~apple~CloudDocs/DEV/Podzemelya/userdata', 'w') as userdata:
+    with shelve.open('userdata', 'r') as userdata:
         entry = userdata[f'{message.from_user.id}']
     bot.send_message(
         message.chat.id, f'{message.from_user.id} \r\nUserdata: {entry}', parse_mode='Html')
